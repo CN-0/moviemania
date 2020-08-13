1c35a3177949c3a95cf5027471674e0deff1ce9e
unknown <codingnoob0@gmail.com>
2020-08-13 19:34:15 +0530

    be2

--- server/index.js
+++ server/index.js
@@ -9,13 +9,10 @@ app.use(express.json())
 app.use(cors())
 app.use('/users',userRouter)
 
+app.use(express.static(__dirname + '/dist/first-app'));
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(__dirname + '/dist/first-app'));
-
-    app.get('/*', (req,res)=>{
-        res.sendFile(path.join(__dirname + '/dist/first-app/index.html'))
-    })
-}
+app.get('/*', (req,res)=>{
+    res.sendFile(path.join(__dirname + '/dist/first-app/index.html'))
+})
 
 app.listen(process.env.PORT || 5000, () => console.log(`Listening Socket on ${ process.env.PORT || 5000 }`));
