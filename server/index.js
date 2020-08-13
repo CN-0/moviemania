a73cb0b79a6c158e608f5c1cd5f846385e69530e
unknown <codingnoob0@gmail.com>
2020-08-13 19:33:16 +0530

    be

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+require('./db/mongoose')
+const userRouter = require('./routes/user')
+
+app.use(express.json())
+app.use(cors())
+app.use('/users',userRouter)
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(__dirname + '/dist/first-app'));
+
+    app.get('/*', (req,res)=>{
+        res.sendFile(path.join(__dirname + '/dist/first-app/index.html'))
+    })
+}
+
+app.listen(process.env.PORT || 5000, () => console.log(`Listening Socket on ${ process.env.PORT || 5000 }`));
