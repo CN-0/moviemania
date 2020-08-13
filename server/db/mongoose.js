f9503565532380c666c175f2a4ea020e02a37fbf
unknown <codingnoob0@gmail.com>
2020-08-13 18:44:23 +0530

    noob

--- server/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect( process.env.MONGODB_URI ,{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    autoIndex:true,
-    useUnifiedTopology:true,
-    useFindAndModify:false
-})
